a35542cdc8eccb9e72988e5230ea2804c99bd6de
Yuriy Podnebesov <dartwader032@gmail.com>
2020-01-22 11:30:47 +0200

    accept changes

--- test.cpp
+++ test.cpp
@@ -7,12 +7,9 @@ int main()
     cout<<"Hello world!\n";
     int x = 0;
     int y = 49;
-<<<<<<< HEAD
-=======
     for(int i = 0; i < 30; i++)
     {
         cout<<"i = "<<i<<endl;
     }
->>>>>>> dev
     return 0;
 }
